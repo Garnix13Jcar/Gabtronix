f347ea71bc2ae7d64844f6caab309e13424ee2d3
Garnix13Jcar <56598716+Garnix13Jcar@users.noreply.github.com>
2019-10-22 10:35:49 -0500

    Add files via upload

---
+++ 05_if-VLAN- MAYOR DE EDAD.py
@@ -0,0 +1,14 @@
+Nombre = input("gregar nombre")
+Apellido = input("agregar apellido")
+Ubicacion = input("agregar ubicacion")
+Edad =input ("agregar edad")
+
+edad =input ("agregar edad")
+
+if edad >=  "18":
+    print("mayor de edad") 
+
+else:
+    print ("menor de edad")
+    
+print("hola te llamas",Nombre,"tu apellido es",Apellido,"estas ubicado en",Ubicacion,"tienes la edad de",Edad,"y ademas eres mayor de edad",)
