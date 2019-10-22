f0b28d990e38435721e97285e86cc38b396aef19
Garnix13Jcar <56598716+Garnix13Jcar@users.noreply.github.com>
2019-10-22 10:54:20 -0500

    Add files via upload

---
+++ 06_if-par-impar.py
@@ -0,0 +1,9 @@
+numero= int (input(" agregar numero  "))
+if numero ==0:
+    print("numero nulo")
+elif numero %2:
+    print("numero agregado es impar")
+else : 
+    print("numero agregado es par")
+    
+    
